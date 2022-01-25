0d10cfcad7304ea5504fc5a75c95e4a41da986b8
randomblue <jpmedallon@gmail.com>
2022-01-24 16:57:04 -0800

    fixed the images to flow nicely sized in the app

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
 
-
 class App extends React.Component {
     state = { images: [] };
 
